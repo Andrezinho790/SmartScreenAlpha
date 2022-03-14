e966d42748cfc6b60d13fa3345b023d5fedbc593
Andrezinho790 <50087071+Andrezinho790@users.noreply.github.com>
2022-03-14 17:57:27 -0300

    test with add data

--- js/script.js
+++ js/script.js
@@ -9,5 +9,5 @@ document.addEventListener('keydown', (event) => {
     var name = event.key;
     var code = event.code;
     // Alert the key name and key code on keydown
-    alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+    //util_regression.addData
   }, false);
