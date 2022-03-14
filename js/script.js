43ca3b5dc5b78eaca56fc93ec3344db117568f70
Andrezinho790 <50087071+Andrezinho790@users.noreply.github.com>
2022-03-14 19:32:09 -0300

    trying to dispach event from another script

--- js/script.js
+++ js/script.js
@@ -9,5 +9,5 @@ document.addEventListener('keydown', (event) => {
     var name = event.key;
     var code = event.code;
     // Alert the key name and key code on keydown
-    //util_regression.addData
+    document.dispatchEvent(gazeMoveEvent);
   }, false);
