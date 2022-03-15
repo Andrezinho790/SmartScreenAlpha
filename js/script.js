103db878b88621aabd728869d3f750f917f4036b
Andrezinho790 <50087071+Andrezinho790@users.noreply.github.com>
2022-03-14 21:54:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
 webgazer.setGazeListener((data,timestamp) =>{
-    console.log(data,timestamp)
+    //console.log(data,timestamp)
 }).begin()
 
 document.addEventListener('keydown', (event) => {
